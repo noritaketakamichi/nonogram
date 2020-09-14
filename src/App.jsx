d1f80aa7cb2ea522ed3856c6701f99d024c5cb6c
noritaketakamichi <noritaket28@gmail.com>
2020-09-14 16:05:22 +0900

    add numbers

--- src/App.jsx
+++ src/App.jsx
@@ -15,14 +15,26 @@ function App() {
 		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-  ]);
-  
-  const[checkedList,setCheckedList]=useState([]);
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	]);
+
+	const [checkedList, setCheckedList] = useState([]);
+
+  //[上の数字][下の数字]
+	const [numbers, setNumbers] = useState([
+		['0', '1', '8', '9', '2 3', '1 3 2 2 2', '5', '3', '1', '0'],
+		['1　1', '5', '2', '2', '2', '6', '7', '6', '1　1', '1　1'],
+	]);
 
 	return (
 		<div>
-			<Board picArray={picArray} setPicArray={setPicArray} checkedList={checkedList} setCheckedList={setCheckedList}/>
+			<Board
+				picArray={picArray}
+				setPicArray={setPicArray}
+				checkedList={checkedList}
+        setCheckedList={setCheckedList}
+        numbers={numbers}
+			/>
 		</div>
 	);
 }
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -5,24 +5,42 @@ import classNames from 'classnames';
 
 function Board(props) {
 	return (
-			<div className="board">
-        {/* チェックされた箇所のリスト */}
-				{props.checkedList.map((elm) => {
-					return <div className={classNames('checked',`x${elm[0]}`,`y${elm[1]}`)}></div>;
-				})}
-				{props.picArray.map((elm, index) => {
-					return (
-						<BoardBar
-							xPos={index}
-							boardArray={elm}
-							picArray={props.picArray}
-							setPicArray={props.setPicArray}
-							checkedList={props.checkedList}
-							setCheckedList={props.setCheckedList}
-						/>
-					);
-				})}
-			</div>
+		<div className="board">
+			{/* チェックされた箇所のリスト */}
+			{props.checkedList.map((elm) => {
+				return <div className={classNames('checked', `x${elm[0]}`, `y${elm[1]}`)}></div>;
+			})}
+
+			{/* マスのリスト */}
+			{props.picArray.map((elm, index) => {
+				return (
+					<BoardBar
+						xPos={index}
+						boardArray={elm}
+						picArray={props.picArray}
+						setPicArray={props.setPicArray}
+						checkedList={props.checkedList}
+						setCheckedList={props.setCheckedList}
+					/>
+				);
+			})}
+			{/* 上と左の数字 */}
+			{props.numbers[0].map((elm, index) => {
+				return (
+					<div className={classNames('upperBar', `upper${index}`)}>
+						<p className="upperNumbers">{elm}</p>
+					</div>
+				);
+			})}
+
+			{props.numbers[1].map((elm, index) => {
+				return (
+					<div className={classNames('leftBar', `left${index}`)}>
+						<p className="leftNumbers">{elm}</p>
+					</div>
+				);
+			})}
+		</div>
 	);
 }
 
