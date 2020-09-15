1110d692cf32edc5ed5977fca8b49ab1c5518e14
noritaketakamichi <noritaket28@gmail.com>
2020-09-15 16:36:09 +0900

    move to other page

--- src/components/Picture.jsx
+++ src/components/Picture.jsx
@@ -1,13 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import '../Picture.scss';
 import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
 function Picture(props) {
-
+	
+	const history = useHistory();
 	return (
-		<div className="pictureflame">
+		<div className="pictureflame" >
 			<p>{props.picture.name}</p>
             <p>made by {props.picture.author}</p>
+			<div className="playCard" onClick={(e) => {
+			history.push(`/${props.picture.id}/`);
+		}}><p className="playString">PLAY THIS</p></div>
 		</div>
 	);
 }
