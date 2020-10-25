7a879730bf196cde5da9277db511b1143fc4bd4d
noritaketakamichi <noritaket28@gmail.com>
2020-10-25 19:35:53 +0900

    line css

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,59 +1,79 @@
-import React from 'react';
-import '../App.scss';
-import BoardBar from './BoardBar';
-import classNames from 'classnames';
+import React from "react";
+import "../App.scss";
+import BoardBar from "./BoardBar";
+import classNames from "classnames";
 
 function Board(props) {
-	return (
-		<div>
-			{/* チェックされた箇所のリスト */}
-			{props.checkedList.map((elm) => {
-				let elemType;
-				if (elm[2] === 0) {
-					elemType = 'whiteElem';
-				} else if (elm[2] === 2) {
-					elemType = 'batsuElem';
-				} else {
-					elemType = 'blackElem';
-				}
+  return (
+    <>
+      {/* チェックされた箇所のリスト */}
+      {props.checkedList.map((elm) => {
+        let elemType;
+        if (elm[2] === 0) {
+          elemType = "whiteElem";
+        } else if (elm[2] === 2) {
+          elemType = "batsuElem";
+        } else {
+          elemType = "blackElem";
+        }
 
-				return <div className={classNames(elemType, `x${elm[0]}`, `y${elm[1]}`)}></div>;
-			})}
+        return (
+          <div
+            className={classNames(elemType, `x${elm[0]}`, `y${elm[1]}`)}
+          ></div>
+        );
+      })}
 
-			{/* マスのリスト */}
-			{props.picArray.map((elm, index) => {
-				return (
-					<BoardBar
-						yPos={index}
-						boardArray={elm}
-						picArray={props.picArray}
-						setPicArray={props.setPicArray}
-						checkedList={props.checkedList}
-						setCheckedList={props.setCheckedList}
-						answerPic={props.answerPic}
-						selectedIcon={props.selectedIcon}
-						picName={props.picName}
-					/>
-				);
-			})}
-			{/* 上と左の数字 */}
-			{props.numbers[0].map((elm, index) => {
-				return (
-					<div className={classNames('upperBar', `upper${index}`)}>
-						<p className="upperNumbers">{elm}</p>
-					</div>
-				);
-			})}
+      {/* マスのリスト */}
+      {props.picArray.map((elm, index) => {
+        return (
+          <BoardBar
+            yPos={index}
+            boardArray={elm}
+            picArray={props.picArray}
+            setPicArray={props.setPicArray}
+            checkedList={props.checkedList}
+            setCheckedList={props.setCheckedList}
+            answerPic={props.answerPic}
+            selectedIcon={props.selectedIcon}
+            picName={props.picName}
+          />
+        );
+      })}
+      {/* 上と左の数字 */}
+      {/* 上 */}
 
-			{props.numbers[1].map((elm, index) => {
-				return (
-					<div className={classNames('leftBar', `left${index}`)}>
-						<p className="leftNumbers">{elm}</p>
-					</div>
-				);
-			})}
-		</div>
-	);
+      {props.numbers[0].map((elm, index) => {
+        return (
+          //   <div className="upperBarContainer">
+          <div
+            className={classNames(
+              "upperBarContainer",
+              "upperBar",
+              `upper${index}`
+            )}
+          >
+            <div>
+              {/* <p className="upperNumbers">{elm}</p> */}
+              {elm.split(" ").map((num) => {
+                return <p　className="upperLine">{num}</p>;
+              })}
+            </div>
+          </div>
+          //   </div>
+        );
+      })}
+
+      {/* 左	 */}
+      {props.numbers[1].map((elm, index) => {
+        return (
+          <div className={classNames("leftBar", `left${index}`)}>
+            <p className="leftNumbers">{elm}</p>
+          </div>
+        );
+      })}
+    </>
+  );
 }
 
 export default Board;
